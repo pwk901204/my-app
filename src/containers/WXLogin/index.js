b21ada2dbb8d3c438da696cdf66ea74ca93bd70c
Kkk <Kkk@KkkdeiMac.local>
2017-12-01 17:51:01 +0800

    xx

--- src/containers/WXLogin/index.js
+++ src/containers/WXLogin/index.js
@@ -4,7 +4,7 @@ import {connect} from "react-redux";
 class WXLogin extends Component {
 	componentDidMount(){
 		if(!this.props.userInfo.auth){
-  			window.location.href="https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx2567a448e97bfc85&redirect_uri="+ encodeURIComponent(window.location.origin + "/HomePage") +"&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect";
+  			window.location.href="https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx2567a448e97bfc85&redirect_uri="+ encodeURIComponent(window.location.origin + "/mobile") +"&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect";
   		}else{
   			global.customizeHistory.push("/")
   		}
